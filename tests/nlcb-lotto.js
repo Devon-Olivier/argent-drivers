7d8922d18e760a10f6feefbe0fcdef0987573c6d
Devon Olivier <medevon@gmail.com>
2012-12-24 04:09:39 -0400

    add lottoplus/nlcb test

---
+++ tests/nlcb-lotto.js
@@ -0,0 +1,12 @@
+DRIVERS = require('../argent-drivers.js');
+console.log(DRIVERS);
+
+lottoplusNlcbDriver = DRIVERS.lottoplus.nlcb.createDriver();
+console.log(lottoplusNlcbDriver);
+lottoplusNlcbDriver.getDrawNumberRange({start:1, end:2}, function drawNumberRangeCallback(error, draws){
+  if(error !== null){
+    console.log(error);
+    return;
+  }
+  console.log(draws);
+});
