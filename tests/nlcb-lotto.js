b09d4b779cdae060b9c271434cddc98d4330b36d
Devon Olivier <medevon@gmail.com>
2013-01-04 04:17:59 -0400

    add test

--- tests/nlcb-lotto.js
+++ tests/nlcb-lotto.js
@@ -10,3 +10,16 @@ lottoplusNlcbDriver.getDrawNumberRange({start:1, end:2}, function drawNumberRang
   }
   console.log(draws);
 });
+
+var moment = DRIVERS.moment;
+var range = {
+  start: moment('2012 12 1', 'YYYY MM DD'),
+  end: moment('2013 1 1', 'YYYY MM DD')
+};
+lottoplusNlcbDriver.getDrawDateRange(range, function drawDateRangeCallback(error, draws){
+  if(error !== null){
+    console.log(error);
+    return;
+  }
+  console.log(draws);
+});
