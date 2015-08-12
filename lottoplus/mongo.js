25399b766bbf6fcb08a9771aa8f9b0f7689aa564
Devon Olivier <medevon@gmail.com>
2015-08-12 17:10:01 -0400

    add interface for storing and retreiving lottoplus jackpot stats

--- lottoplus/mongo.js
+++ lottoplus/mongo.js
@@ -18,6 +18,7 @@ const LODASH = require('lodash');
 const TYPE = require('./type.js');
 const MONGOCONF = require('../config/mongo-conf.json');
 
+//TODO: IMPROVE ERROR REPORTING AND DETECTING
 
 //TODO: convert table.js to object oriented module. It is more functional now
 //but it is jus message passing really isn't it? Look at how it is 
@@ -41,22 +42,6 @@ const MAKETABLE = require('./table.js').makeTable;
 
 
 const installMongoGet = function installMongoGet(table) {
-
-  /**
-   * mongoDrawToNlcbDraw: draw from mongo database -> Draw
-   *
-   * consume a draw from the mongodb store of draws and return a
-   * draw as described above.
-   *
-   * @param {mongoDraw} a draw from a mongodb store of draws
-   * @return {Draw}
-   * @api private
-   **/
-  const mongoDrawToNlcbDraw = function mongoDrawToNlcbDraw(mongoDraw) {
-    const nlcbDraw = LODASH.omit(mongoDraw, '_id');
-    nlcbDraw.drawNumber = mongoDraw._id;
-    return nlcbDraw;
-  };
 
   /**
    * getOne: uri, queryObject -> promise for a Draw
@@ -251,6 +236,21 @@ const nlcbDrawToMongoDraw = function nlcbDrawToMongoDraw(nlcbDraw) {
   return mongoDraw;
 };
 
+/**
+ * mongoDrawToNlcbDraw: draw from mongo database -> Draw
+ *
+ * consume a draw from the mongodb store of draws and return a
+ * draw as described above.
+ *
+ * @param {mongoDraw} a draw from a mongodb store of draws
+ * @return {Draw}
+ * @api private
+ **/
+const mongoDrawToNlcbDraw = function mongoDrawToNlcbDraw(mongoDraw) {
+  const nlcbDraw = LODASH.omit(mongoDraw, '_id');
+  nlcbDraw.drawNumber = mongoDraw._id;
+  return nlcbDraw;
+};
 
 /**
  * saveOneDraw: uri, Draw -> Promise for a native mongodb driver resultObject
@@ -326,7 +326,7 @@ const saveManyDraws = function saveManyDraws (uri, draws) {
  * removeOneDraw: uri, Draw -> Promise for a native mongodb driver resultObject
  *
  * consume a mongodb uri specifying a database and a Draw and return a
- * Promise for a mongodb native drive resultObject 
+ * Promise for a mongodb native driver resultObject 
  *
  * @param {uri} the uri specifying the database to use
  * @param {draws} a Draw 
@@ -355,59 +355,215 @@ const removeOneDraw = function removeOneDraw (uri, nlcbDraw) {
     });
 }; 
 
-const saveNewJackpot = function saveNewJackpot (uri, jackpot) {
+/**
+ * calculateHighestJackpot: uri, newJackpotInfo -> Promise for an object 
+ *
+ * consume a mongodb uri specifying a database and an object specifying the 
+ * draw number and value of the new estimated jackpot for the next lottoplus 
+ * draw. return a Promise for a highest jackpot object
+ *
+ * The highest jackpot object:
+ *  {
+ *    drawNumber: <number> draw number with highest jackpot
+ *    jackpot: <number> highest jackpot
+ *  }
+ *
+ * If newJackpotInfo is null or undefined then the mean is derived from the 
+ * database only. Else newJackpotInfo is used in calculating the highest
+ * together with the database.
+ *
+ * @param {uri} the uri specifying the database to use
+ * @param {newJackpotInfo} object {drawNumber: <number>, jackpot: <number>} 
+ * @return {Promise} a Promise for a highest jackpot object
+ * @api private
+ **/
+const calculateHighestJackpot = function(uri, newJackpotInfo) {
+  debugLog('calculateHighestJackpot: new jackpot info is: ',
+      newJackpotInfo);
+  debugLog('calculateHighestJackpotInfo: connecting to ', uri);
   return MONGOCLIENT.connect(uri)
-    .then(function (db) {
-      debugLog('got db');
-      const collection = db.collection('lottoplus-stats');
-      debugLog('got draws collection');
-      collection.removeMany.bind(collection);
-      db.collection('stats').update({about: 'jackpot'},
-          {$set:{newJackpot: jackpot}},
-          {upsert: true},
-          function (error, result) {
-            if (error) {
-              deferred.reject(error);
-            }
-            else {
-              deferred.resolve(result);
-            }
-            db.close();
-          });
-      return deferred.promise;
+    .then(function(db) {
+      debugLog('calculateHighestJackpotInfo: connected to ', uri);
+      return db.collection('draws')
+        .find({}, {limit: 1, sort: {jackpot: -1}, fields: {_id: true, jackpot: true}})
+        .next()
+        .then(function (highestJackpotInfo) {
+          db.close();
+
+          debugLog('calculateHighestJackpotInfo: highest jackpot info from database: ', 
+              highestJackpotInfo);
+          if(newJackpotInfo && newJackpotInfo.jackpot > highestJackpotInfo.jackpot){
+            return newJackpotInfo
+          }
+          return mongoDrawToNlcbDraw(highestJackpotInfo);
+        });
+    }).catch(function(error) {
+      debugError(error);
+      db.close();
+      throw error;
     });
 };
 
-const getNewJackpot = function getNewJackpot(uri) {
-  return Q.ninvoke(
-      MONGODB.MongoClient,
-      'connect',
-      MONGOURI,
-      {w:1})
-  .then(function (db) {
-    var deferred = Q.defer();
-    db.collection('stats').findOne({about: 'jackpot'}, function (error, stats) {
-      if (error) {
-        deferred.reject(error);
-      }
-      else {
-        if (stats) {
-          deferred.resolve(stats.newJackpot);
-        }
-        else {
-          deferred.resolve(null);
-        }
-      }
+//TODO: getStandardDeviationJackpot = functino getStandardDeviationJackpot(uri){};
+
+/**
+ * calculateMeanJackpot: uri, newJackpotInfo -> Promise for an object 
+ *
+ * consume a mongodb uri specifying a database and an object specifying the 
+ * draw number and value of the new estimated jackpot for the next lottoplus 
+ * draw. return a Promise for the mean jackpot. 
+ *
+ * If newJackpotInfo is null or undefined then the mean is derived from the 
+ * database only. Else newJackpotInfo is used in calculating the statistics
+ * together with the database.
+ *
+ * @param {uri} the uri specifying the database to use
+ * @param {newJackpotInfo} object {drawNumber: <number>, jackpot: <number>} 
+ * @return {Promise} a Promise for the mean jackpot 
+ * @api private
+ **/
+const calculateMeanJackpot = function calculateMeanJackpot(uri, newJackpotInfo) {
+  debugLog('calculateMeanJackpot: connecting to ', uri);
+  return MONGOCLIENT.connect(uri)
+    .then(function(db) {
+      debugLog('calculateMeanJackpot: connected to ', uri);
+      return db.collection('draws')
+        .aggregate([
+            {$group: {
+                       _id: null, 
+                       meanJackpot: {$avg: '$jackpot'},
+                       count: {$sum: 1}
+                     }}
+        ])
+        .next()
+        .then(function (meanJackpotInfo) {
+          db.close();
+
+          debugLog('calculateMeanJackpot: mean jackpot info from database: ',
+              meanJackpotInfo);
+          const meanJackpot = meanJackpotInfo.meanJackpot;
+          if(newJackpotInfo) {
+            let oldMean = meanJackpot;
+            let n = meanJackpotInfo.count;
+            let newJackpot = newJackpotInfo.jackpot;
+            let newMean = (oldMean * n + newJackpot)/(n+1);
+            return LODASH.round(newMean, 2);
+          }
+          return LODASH.round(meanJackpot, 2);
+        });
+    }).catch(function(error) {
+      debugError(error);
       db.close();
+      throw error;
     });
-    return deferred.promise;
+};
+
+/**
+ * calculateJackpotStats: uri, newJackpotInfo -> Promise for an object 
+ *
+ * consume a mongodb uri specifying a database and an object specifying the 
+ * draw number and value of the new estimated jackpot for the next lottoplus 
+ * draw. Calculates the jackpot statistics and return a Promise for an object 
+ * with the stats.
+ * 
+ * The jackpot stats object: 
+ * {
+ *  next: {
+ *    drawNumber: <number> draw number for next draw
+ *    jackpot: <number> estimated jackpot for next draw
+ *  },
+ *  highest: {
+ *     drawNumber: <number> the draw number of the highest jackpot
+ *     jackpot: <number> the highest jackpot
+ *  },
+ *  meanJackpot: <number> the mean jackpot}
+ *
+ * If newJackpotInfo is null or undefined then the statistics are derived from
+ * the database only. Else newJackpotInfo is used in calculating the statistics
+ * together with the database.
+ *
+ * @param {uri} the uri specifying the database to use
+ * @param {newJackpotInfo} object {drawNumber: <number>, jackpot: <number>} 
+ * @return {Promise} a Promise for a jackpot stats object
+ * @api private
+ **/
+const calculateJackpotStats = function calculateJackpotStats(uri, newJackpotInfo) {
+  return calculateHighestJackpot(uri, newJackpotInfo)
+    .then(function(highestJackpotInfo) {
+      return calculateMeanJackpot(uri, newJackpotInfo)
+        .then(function (meanJackpot) {
+          var stats = {
+            highest: highestJackpotInfo,
+            mean: meanJackpot
+          };
+
+          if(newJackpotInfo) {
+            stats.next = newJackpotInfo;
+          }
+          return stats;
+        });
+    });
+};
+
+/**
+ * updateJackpotStats: uri, newJackpotInfo -> Promise for a native mongodb driver resultObject
+ *
+ * consume a mongodb uri specifying a database and an object specifying the draw number and
+ * value of the new estimated jackpot for the next lottoplus draw.
+ *
+ * Updates the jackpot statistics  and return a Promise for a mongodb native drive resultObject.
+ * If newJackpotInfo is null or undefined then the statistics are derived from the database only.
+ * Else the newJackpotInfo is used in computing the statistics together with the database.
+ *
+ * @param {uri} the uri specifying the database to use
+ * @param {newJackpotInfo} object {drawNumber: <number>, jackpot: <number>} 
+ * @return {Promise} a Promise for a resultObject
+ * @api public
+ **/
+const updateJackpotStats = function updateJackpotStats (uri, newJackpotInfo) {
+  return calculateJackpotStats(uri, newJackpotInfo)
+    .then(function(jackpotStats) {
+      debugLog('updateJackpotStats: ', jackpotStats);
+      return MONGOCLIENT.connect(uri)
+        .then(function(db) {
+          jackpotStats._id = 'jackpot';
+          return db.collection('lottoplusStats')
+            .updateOne({_id: 'jackpot'}, jackpotStats, {w:1, upsert: true})
+        });
+    })
+  .catch(function(error) {
+    debugError(error);
+    db.close();
+    throw error
   });
+};
+
+const getJackpotStats = function getJackpotStats(uri) {
+  debugLog('getJackpotStats: connecting to ', uri);
+  return MONGOCLIENT.connect(uri)
+    .then(function (db) {
+      debugLog('getJackpotStats: connected to ', uri);
+      return db.collection('lottoplusStats')
+        .findOne({_id: 'jackpot'}, {_id:false})
+        .then(function(stats) {
+          db.close();
+          debugLog('getJackpotStats: stats: ', stats);
+          return stats;
+        });
+    }).catch(function(error) {
+      debugError(error);
+      db.close();
+      throw error;
+    });
 };
 
 var debugLog = function () {};
 var debugError = function () {};
 var MONGOURI = MONGOCONF.uri;
 module.exports = function(debug) {
+  //TODO: add a 'test' mode so that we don't have
+  //to be forced to see debug information just to
+  //test the module
   if(debug) {
     debugLog = console.log.bind(console);
     debugError = console.error.bind(console);
@@ -421,6 +577,13 @@ module.exports = function(debug) {
 module.exports.getDraw = getDraw;
 
 module.exports.removeOneDraw = function(query) {
+  //TODO: consider making this less general and
+  //require only a draw number specifying the
+  //draw to delete. Using a query object can be
+  //dangerous.
+  //Also, deleting draws permanently is probably
+  //a bad idea. Maybe we should store deleted
+  //draws into another collection.
   return removeOneDraw(MONGOURI, query);
 };
 
@@ -432,5 +595,9 @@ module.exports.saveOneDraw = function (draws) {
   return saveOneDraw(MONGOURI, draws);
 };
 
-module.exports.getNewJackpot = getNewJackpot;
-module.exports.saveNewJackpot = saveNewJackpot;
+module.exports.getJackpotStats = function() {
+  return getJackpotStats(MONGOURI);
+};
+module.exports.updateJackpotStats = function (jackpotInfo) {
+  return updateJackpotStats(MONGOURI, jackpotInfo);
+};
